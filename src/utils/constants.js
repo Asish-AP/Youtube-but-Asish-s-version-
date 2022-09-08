4ea8f7c40dbfb2c462c00dba0ee5f7abf72a7b4c
Asish Panda <asishpandda@gmail.com>
2022-09-08 21:47:26 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,45 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import JavascriptRoundedIcon from '@mui/icons-material/JavascriptRounded';
+import HtmlIcon from '@mui/icons-material/Html';
+import CssIcon from '@mui/icons-material/Css';
+import CodeOffRoundedIcon from '@mui/icons-material/CodeOffRounded';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+    { name: 'New', icon: <HomeIcon />, },
+    { name: 'JavaScript', icon: <JavascriptRoundedIcon />, },
+    { name: 'HTML', icon: <HtmlIcon />, },
+    { name: 'CSS', icon: <CssIcon />, },
+    { name: 'ReactJS', icon: <CodeOffRoundedIcon />, },
+    { name: 'Arijit Singh', icon: <MusicNoteIcon /> },
+    { name: 'DSA', icon: <SchoolIcon />, },
+    { name: 'Podcast', icon: <GraphicEqIcon />, },
+    { name: 'Movie', icon: <OndemandVideoIcon />, },
+    { name: 'Gaming', icon: <SportsEsportsIcon />, },
+    { name: 'Live', icon: <LiveTvIcon />, },
+    { name: 'Sport', icon: <FitnessCenterIcon />, },
+    { name: 'Fashion', icon: <CheckroomIcon />, },
+    { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+    { name: 'Comedy', icon: <TheaterComedyIcon />, },
+    { name: 'Gym', icon: <FitnessCenterIcon />, },
+    { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ytimg.com/vi/K69tbUo3vGs/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAvFG5Tg2dqYnZ0je4FYYuu2mGMGQ';
+export const demoChannelUrl = 'channel/UC-lTO5rbkJmgHS9AGzQJpsQ';
+export const demoVideoUrl = 'video/K69tbUo3vGs';
+export const demoChannelTitle = 'Visual Escape';
+export const demoVideoTitle = 'Lord Of the rings || Concerning Hobbits BGM';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+    params: {
+        maxResults: 50,
+    },
+    headers: {
+        'X-RapidAPI-Key': '4a40f0c654msh888909c14d6f1a3p1f7a63jsn94223afedf66',
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    },
+};
+
+export const fetchFromAPI = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+    return data;
+};
